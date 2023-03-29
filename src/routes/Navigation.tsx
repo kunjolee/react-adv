f06a283b80eed33b4d37c12dce9a0600badf3674
kunjolee <josuelee28@gmail.com>
2023-03-28 22:24:56 -0600

    order code

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,41 +1,49 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  NavLink,
-  Navigate
-} from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
 import logo from '../logo.svg';
 
 export const Navigation = () => {
-  return (
-    <Router>
-      <div className='main-layout'>
-        <nav>
-            <img src={ logo } alt='React Logo' />
-          <ul>
-            <li>
-              <NavLink to='/home' className={({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to='/about' className={({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
-            </li>
-            <li>
-              <NavLink to='/users' className={({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
-            </li>
-          </ul>
-        </nav>
+    return (
+        <Router>
+            <div className='main-layout'>
+                <nav>
+                    <img src={logo} alt='React Logo' />
+                    <ul>
+                        <li>
+                            <NavLink
+                                to='/home'
+                                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to='/about'
+                                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                            >
+                                About
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to='/users'
+                                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                            >
+                                Users
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
 
-        <Routes>
-          <Route path='/home' element={ <span>Home</span> } />
-          <Route path='/about' element={ <span>About</span> } />
-          <Route path='/users' element={ <span>Users</span> } />
+                <Routes>
+                    <Route path='/home' element={<span>Home</span>} />
+                    <Route path='/about' element={<span>About</span>} />
+                    <Route path='/users' element={<span>Users</span>} />
 
-          <Route path='/*' element={<Navigate to='/home' replace />}/>
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+                    <Route path='/*' element={<Navigate to='/home' replace />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
